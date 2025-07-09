fd05515711610316bc3a529006bf16f5665bc8f4
Tun <lyheanghengcalming@gmail.com>
2025-07-10 00:20:26 +0700

    Add html files

--- Application/urls.py
+++ Application/urls.py
@@ -20,8 +20,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
+    path('', views.index, name='index'), 
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
 ]
